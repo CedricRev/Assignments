afb9100a338b16f7afa7f1cc652805ffaf63c3f8
CedricRev <53487258+CedricRev@users.noreply.github.com>
2022-01-14 21:01:30 +0800

    Create ArrayStack1.java

---
+++ ArrayStack1.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class ArrayStack1 {
+    private final int capacity = 5;
+    private final Object[] data = new Object[capacity];
+    private int t = -1;
+    private int length = 0;
+    
+    public void push(Object value) {
+        if(length + 1 <= capacity) {
+            length++;
+            t++;
+            data[t] = value;
+        } else {
+            throw new IllegalStateException("Stack is full");
+        }
+    }
+    
+    public Object pop() {
+        Object value = data[t];
+        data[t] = null;
+        t--;
+        length--;
+        return value;
+    }
+    
+    public Object peek() {
+        if(isEmpty())
+            return null;
+        else
+            return data[t];
+    }
+    
+    public boolean isEmpty() {
+        return length == 0;
+    }
+    
+    public int length() {
+        return length;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+        ArrayStack stack = new ArrayStack();
+        for(String split: input) {
+            int value = Integer.parseInt(split);
+            stack.push(value);
+        }
+        System.out.println(stack.pop() + " " + stack.peek() + " " + stack.length());
+    }
+}
